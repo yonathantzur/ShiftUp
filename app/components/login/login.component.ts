179a3e8da6265c2b6afe8641da0d228cdfb6b59d
Nofar <nofarisraeli7@gmail.com>
2019-02-17 22:47:44 +0200

    tmp

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {LoginService} from '../../services/login/login.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import {HttpClient} from '@angular/common/http';
+import {debug} from "util";
 
 @Component({
     selector: 'login',
@@ -10,10 +13,16 @@ import {HttpClient} from '@angular/common/http';
     styleUrls: ['./login.css']
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit{
     user: any = {};
+    registerForm: FormGroup;
+    submitted = false;
 
     constructor(private loginService: LoginService) {}
+
+    ngOnInit() {
+        console.error(this);
+    }
 
     onSubmit(loginForm: NgForm) {
         console.log(loginForm.value);
