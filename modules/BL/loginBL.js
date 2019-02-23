e7836aaf4cf673df1309c324b0be83002073facc
Nofar <nofarisraeli7@gmail.com>
2019-02-24 01:41:34 +0200

    support JWT in login

--- modules/BL/loginBL.js
+++ modules/BL/loginBL.js
@@ -1,25 +1,35 @@
 const DAL = require('../DAL');
 const config = require('../../config');
+const Hash = require('../libs/hash');
+const JWT = require('../libs/jwt');
 
 const usersCollectionName = config.db.collections.users;
 
 module.exports = {
-
-    UserLogin(userData) {
+    UserLogin: (userData) => {
         return new Promise((resolve, reject) => {
-            console.log(userData);
             DAL.FindOne(usersCollectionName, {email: userData.email})
-                .then((user)=>{
-                    if (user instanceof Object && user.password === userData.password){
-                            resolve(user);
+                .then((user) => {
+                    if (user instanceof Object && user.salt && user.password === Hash.hash(userData.password, user.salt).hash) {
+                        try {
+                            const token = JWT.sign({
+                                i: user._id,
+                                s: user.salt,
+                            });
+                            // resolve(user);
+                            resolve(token);
+                        } catch (e) {
+                            console.error(e);
+                            reject('token generate failed')
+                        }
                     } else {
                         reject('user not match the password')
                     }
                 })
-                .catch((err)=>{
+                .catch((err) => {
                     reject('user not found');
                 });
         });
-    }
+    },
 
 };
--- modules/routes/login.js
+++ modules/routes/login.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const loginBL = require('../BL/loginBL');
 const config = require('../../config');
+const JWT = require('../libs/jwt');
 
 // var prefix = prefix = "/login";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.sendStatus(200);
 });
 
@@ -16,14 +17,21 @@ router.post("/userLogin", (req, res) => {
         email: req.body.email,
         password: req.body.password
     };
-    loginBL.UserLogin(userData).then(result => {
+    loginBL.UserLogin(userData).then((result) => {
         res.send(result);
-    }).catch(err => {
+    }).catch((err) => {
         console.error(err);
         res.sendStatus(500);
     });
 
 });
 
+router.post("/userLoginTester", JWT.middleware, (req, res) => {
+    res.json(req.user);
+});
+
+router.get("/userLoginTester", JWT.middleware, (req, res) => {
+    res.json(req.user);
+});
 
 module.exports = router;
