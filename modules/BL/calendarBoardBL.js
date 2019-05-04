48b9c3c5065a28466e7ef711ba64e0ad54ce1339
yonathantzur1 <yonathantzur1@gmail.com>
2019-05-04 18:20:08 +0300

    Minor calendarBoardBL change

--- modules/BL/calendarBoardBL.js
+++ modules/BL/calendarBoardBL.js
@@ -4,6 +4,8 @@ const config = require('../../config');
 
 const usersCollectionName = config.db.collections.users;
 const shiftsCollectionName = config.db.collections.shifts;
+const businessesCollectionName = config.db.collections.businesses;
+const constraintsCollectionName = config.db.collections.constraints;
 
 module.exports = {
     GetShiftsSchedule(businessId, year, month) {
