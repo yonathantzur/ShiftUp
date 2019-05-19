77e5619ae685a3e855a677a066bff2f95452a1ea
yonathantzur1 <yonathantzur1@gmail.com>
2019-05-19 13:50:51 +0300

    Alert schedule on mail

--- modules/mailer.js
+++ modules/mailer.js
@@ -1,6 +1,21 @@
 const nodemailer = require('nodemailer');
 const config = require('../config');
 
+const months = [
+    "ינואר",
+    "פברואר",
+    "מרץ",
+    "אפריל",
+    "מאי",
+    "יוני",
+    "יולי",
+    "אוגוסט",
+    "ספטמבר",
+    "אוקטובר",
+    "נובמבר",
+    "דצמבר"
+]
+
 // Create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport(
     {
@@ -39,8 +54,58 @@ module.exports = {
             GetTimeBlessing() + name + ", אנחנו שמחים לברך אותך על הצטרפותך לאתר ShiftUp!");
     },
 
+    AlertWorkerWithSchedule(email, name, shifts, month, year) {
+        let workerShiftsArray = [];
 
+        Object.keys(shifts).forEach(shiftDate => {
+            workerShiftsArray.push({
+                date: shiftDate,
+                shiftsNames: shifts[shiftDate]
+            });
+        });
+
+        // Sort shifts by date
+        workerShiftsArray = workerShiftsArray.sort((shiftA, shiftB) => {
+            return (shiftA.date > shiftB.date) ? 1 : -1;
+        });
+
+        let scheduleStr = "\n<div style={{titleContainer}}>" +
+            "אלו המשמרות שלך לחודש " + months[parseInt(month) - 1] + " " + year +
+            "</div>\n\n";
+
+        workerShiftsArray.forEach(shift => {
+            scheduleStr += "<div style={{shiftContainer}}>" +
+                formatDate(new Date(shift.date)) +
+                " - " + shift.shiftsNames[0] +
+                "</div>"
+        });
+
+        let css = {
+            titleContainer: '"margin-bottom:5px;"',
+            shiftContainer: '"margin-bottom:3px;"'
+        }
+
+        this.SendMail(email,
+            "ShiftUp",
+            GetTimeBlessing() + name + ",\n" + scheduleStr, css);
+    }
 };
+
+function formatDate(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    return day + "/" + month + "/" + year;
+}
 
 function GetTimeBlessing() {
     let hour = new Date().getHours();
@@ -64,8 +129,13 @@ function GetTimeBlessing() {
 
 function ReplaceStyleCss(html, css) {
     Object.keys(css).forEach(className => {
-        html = html.replace("{{" + className + "}}", css[className]);
+        html = html.replaceAll("{{" + className + "}}", css[className]);
     });
 
     return html;
 }
+
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
