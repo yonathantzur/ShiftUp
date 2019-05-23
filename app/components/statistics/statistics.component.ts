8e534ca4f59481182223c614f380f73fc02d8e3e
nivhojberg <nivhojberg@hotmail.com>
2019-05-24 01:54:06 +0300

    Added salaries chart
    Added salaries workers chart to see how many workers are earning each salary (by range).

--- app/components/statistics/statistics.component.ts
+++ app/components/statistics/statistics.component.ts
@@ -1,29 +1,37 @@
 import { Component } from '@angular/core';
 
+import { UsersService } from '../../services/users/users.service';
 import { BusinessesService } from '../../services/businesses/businesses.service';
 import { WorkersService } from '../../services/workers/workers.service';
 import { ConstraintsService } from '../../services/constraints/constraints.service';
 import { ShiftService } from '../../services/shifts/shifts.service';
 
-// declare function d3(string: any): any;
 declare var d3: any;
+const CHARTS_SIZE: number = 400;
 
 @Component({
     selector: 'statistics',
     templateUrl: './statistics.html',
-    providers: [BusinessesService, WorkersService, ConstraintsService, ShiftService],
+    providers: [
+        UsersService,
+        BusinessesService,
+        WorkersService,
+        ConstraintsService,
+        ShiftService
+    ],
     styleUrls: ['./statistics.css']
 })
 
 export class StatisticsComponent {
+    user: any;
     business: any;
     manager: any;
     workers: Array<any>;
     constraints: Array<any>;
     loadingRequets: number;
 
-
     constructor(
+        private usersService: UsersService,
         private businessesService: BusinessesService,
         private workersService: WorkersService,
         private constraintsService: ConstraintsService,
@@ -44,6 +52,7 @@ export class StatisticsComponent {
             this.workers = workers.filter((worker: any) => !worker.isManager);
         
             this.buildWorkersAgesChart(this.workers);
+            this.buildSalaryChart(this.workers);
 
             this.loadingRequets--;
         });
@@ -127,6 +136,77 @@ export class StatisticsComponent {
             var c = labelArc.centroid(d);
             workersAgesContext.fillText(data[d.index].name, c[0], c[1]);
         });
+    }
+
+    async buildSalaryChart(workers: Array<any>) {
+        var data: any = {
+            x: `שכר`,
+            y: `עובדים`,
+            length: workers.length
+        };
+        workers.forEach((worker: any, index: number) => {
+            data[index] = worker.salary;
+        })
+        
+        var height = CHARTS_SIZE;
+        var width = CHARTS_SIZE;
+        var margin = ({top: 20, right: 20, bottom: 30, left: 40});
+
+        var x = d3.scaleLinear()
+            .domain([0, 100]).nice()
+            .range([margin.left, width - margin.right]);
+        
+        var bins = d3.histogram()
+            .domain(x.domain())
+            .thresholds(x.ticks(20))
+            (data);
+
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(bins, (d: any) => d.length)]).nice()
+            .range([height - margin.bottom, margin.top]);
+
+        const svg: any = d3.select("#workersSalariesChart");
+        
+        const bar = svg.append("g")
+            .attr("fill", "steelblue")
+            .selectAll("rect")
+            .data(bins)
+            .join("rect")
+            .attr("x", (d: any) => x(d.x0) + 1)
+            .attr("width", (d: any) => Math.max(0, x(d.x1) - x(d.x0) - 1))
+            .attr("y", (d: any) => y(d.length))
+            .attr("height", (d: any) => y(0) - y(d.length));
+        
+        var xAxis = (g: any) => g
+            .attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x).tickSizeOuter(0))
+            .call((g: any) => g.append("text")
+            .attr("x", width - margin.right)
+            .attr("y", -4)
+            .attr("fill", "#000")
+            .attr("font-weight", "bold")
+            .attr("text-anchor", "end")
+            .attr("font-size", "12px")
+            .text(data.x));
+
+        var yAxis = (g: any) => g
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(d3.axisLeft(y))
+            .call((g: any) => g.select(".domain").remove())
+            .call((g: any) => g.select(".tick:last-of-type text").clone()
+            .attr("x", 4)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .attr("font-size", "12px")
+            .text(data.y));
+
+        svg.append("g")
+            .call(xAxis);
+        
+        svg.append("g")
+            .call(yAxis);
+        
+        return svg.node();
     }
 }
 
