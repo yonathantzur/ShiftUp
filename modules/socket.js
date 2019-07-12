31c5e099704ade09cefb82582a2df1e16b4e8c2e
yonathantzur1 <yonathantzur1@gmail.com>
2019-07-12 16:16:21 +0300

    Sign in user to socket on login

--- modules/socket.js
+++ modules/socket.js
@@ -1,8 +1,14 @@
+const tokenHandler = require('./handlers/tokenHandler');
+
 module.exports = (io) => {
     io.on('connection', (socket) => {
 
         socket.on('login', () => {
+            // Decode user object from the token.
+            let user = tokenHandler.decodeTokenFromSocket(socket);
 
+            // Join user to socket room by its object id.
+            user && socket.join(user.id);
         });
 
         socket.on('disconnect', () => {
